73d1190ab9bba3b41be048188d0609dcfa18f37f
Javid <javid.abdul2004@gmail.com>
2025-04-27 11:26:20 +0530

    File Structure Organized

--- HealthEcho/src/main/java/com/buddy/HealthEcho/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.buddy.HealthEcho.repo;
-
-import com.buddy.HealthEcho.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
