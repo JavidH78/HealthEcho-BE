b91ab5709c5bb335f2962d531e1243eec1cbab0c
Javid <javid.abdul2004@gmail.com>
2025-04-27 11:37:00 +0530

    File Structure Organized

---
+++ src/main/java/com/buddy/HealthEcho/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.buddy.HealthEcho.controller;
+
+import com.buddy.HealthEcho.model.User;
+import com.buddy.HealthEcho.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService; // Assume you have a UserService to handle user-related logic
+
+    // POST method to register a new user
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
